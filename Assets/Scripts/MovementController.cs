ea1337c262a8d8955757ccc93a36a4778cedb4ff
chandev0908 <chandev0908@gmail.com>
2021-05-27 03:21:50 +0800

    added joystick and joystick movement

--- Assets/Scripts/MovementController.cs
+++ Assets/Scripts/MovementController.cs
@@ -7,57 +7,18 @@ public class MovementController : MonoBehaviour
     public float moveSpeed;
     private Rigidbody rigidBody;
     private Transform transform;
+    protected Joystick joystick;
 
     // Start is called before the first frame update
     void Start()
     {
+        joystick = FindObjectOfType<Joystick>();
         rigidBody = GetComponent<Rigidbody>();
-        transform = GetComponent<Transform>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.W))
-        {
-            transform.Translate(Vector3.forward * Time.deltaTime * moveSpeed);
-        }
-        if (Input.GetKey(KeyCode.A))
-        {
-            transform.Translate(Vector3.left * Time.deltaTime * moveSpeed);
-        }
-        if (Input.GetKey(KeyCode.S))
-        {
-            transform.Translate(Vector3.back * Time.deltaTime * moveSpeed);
-        }
-        if (Input.GetKey(KeyCode.D))
-        {
-            transform.Translate(Vector3.right * Time.deltaTime * moveSpeed);
-        }
-
-
-
-
-        /*
-        if (Input.GetKey(KeyCode.W))
-        {
-            rigidBody.AddForce(0, 0, 1 * Time.deltaTime * moveSpeed);
-        }
-
-        if (Input.GetKey(KeyCode.A))
-        {
-            rigidBody.AddForce(1, 0, 0 * Time.deltaTime * moveSpeed);
-        }
-
-        if (Input.GetKey(KeyCode.S))
-        {
-            rigidBody.AddForce(0, 0, -1 * Time.deltaTime * moveSpeed);
-        }
-
-        if (Input.GetKey(KeyCode.D))
-        {
-            rigidBody.AddForce(-1, 0, 0 * Time.deltaTime * moveSpeed);
-        }
-        */
+        rigidBody.velocity = new Vector3(joystick.Horizontal * moveSpeed, rigidBody.velocity.y, joystick.Vertical * moveSpeed);
     }
 }
